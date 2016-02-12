e594efbb62bec2467964e055e51dc2e07d0c9928
LGremler <logangremler@gmail.com>
2016-02-11 22:52:40 -0600

    Added a way to start the program

---
+++ main.py
@@ -0,0 +1,10 @@
+# This runs the schedule generator. There's literally nothing to this file right
+# now.
+
+# Author: Logan Gremler 2/11/16
+
+from ScheduleWriter import *
+import sys
+import os.path
+
+write_schedule()
