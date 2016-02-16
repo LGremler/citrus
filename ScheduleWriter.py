88470f80fe65f2d313615194369d2333b438b167
Logan Gremler <logangremler@gmail.com>
2016-02-15 22:27:25 -0600

    Updated Output and .sdl Formatting
    Output is now even more visually appealing, and creating multiple .sdl files is now possible as each one is assigned a different week code and given a different name.

--- ScheduleWriter.py
+++ ScheduleWriter.py
@@ -5,44 +5,48 @@
 import sys
 import os.path
 
-# Create a .sdl file:
-document = open('Schedule.sdl', 'a')
-if os.path.exists('./Schedule.sdl'):
-    document = open('Schedule.sdl', 'r+')
+# Get a week code from the user:
+week = raw_input("Enter a numerical week code: ")
+
+# Create and open a new .sdl file:
+document = 'Week' +str(week) +'.sdl'
+schedule = open(document, 'w')
 
 # Take user input:
 print "***Separate tasks with a comma***"
-monday = raw_input("What needs done this Monday?")
-tuesday = raw_input("What needs done this Tuesday?")
-wednesday = raw_input("What needs done this Wednesday?")
-thursday = raw_input("What needs done this Thursday?")
-friday = raw_input("What needs done this Friday?")
-saturday = raw_input("What needs done this Saturday?")
-sunday = raw_input("What needs done this Sunday?")
+monday = raw_input("What needs done this Monday? ")
+tuesday = raw_input("What needs done this Tuesday? ")
+wednesday = raw_input("What needs done this Wednesday? ")
+thursday = raw_input("What needs done this Thursday? ")
+friday = raw_input("What needs done this Friday? ")
+saturday = raw_input("What needs done this Saturday? ")
+sunday = raw_input("What needs done this Sunday? ")
 
-# Separate tasks by replacing commas with blank lines:
-day1 = monday.replace(",", "\n")
-day2 = tuesday.replace(",", "\n")
-day3 = wednesday.replace(",", "\n")
-day4 = thursday.replace(",", "\n")
-day5 = friday.replace(",", "\n")
-day6 = saturday.replace(",", "\n")
-day7 = sunday.replace(",", "\n")
+# Separate tasks by replacing commas with blank lines and adding a "-"
+# to signal the start of a task:
+day1 = "- " + monday.replace(",", "\n- ")
+day2 = "- " + tuesday.replace(",", "\n- ")
+day3 = "- " + wednesday.replace(",", "\n- ")
+day4 = "- " + thursday.replace(",", "\n- ")
+day5 = "- " + friday.replace(",", "\n- ")
+day6 = "- " + saturday.replace(",", "\n- ")
+day7 = "- " + sunday.replace(",", "\n- ")
 
 
 # Write all relevant information to the schedule:
 def write_schedule():
-    header1 = document.write("Monday:\n")
-    task1 = document.write(day1)
-    header2 = document.write("\n\nTuesday:\n")
-    task2 = document.write(day2)
-    header3 = document.write("\n\nWednesday:\n")
-    task3 = document.write(day3)
-    header4 = document.write("\n\nThursday:\n")
-    task4 = document.write(day4)
-    header5 = document.write("\n\nFriday:\n")
-    task5 = document.write(day5)
-    header6 = document.write("\n\nSaturday:\n")
-    task6 = document.write(day6)
-    header7 = document.write("\n\nSunday:\n")
-    task7 = document.write(day7)
+    header1 = schedule.write("Monday:\n")
+    task1 = schedule.write(day1)
+    header2 = schedule.write("\n\nTuesday:\n")
+    task2 = schedule.write(day2)
+    header3 = schedule.write("\n\nWednesday:\n")
+    task3 = schedule.write(day3)
+    header4 = schedule.write("\n\nThursday:\n")
+    task4 = schedule.write(day4)
+    header5 = schedule.write("\n\nFriday:\n")
+    task5 = schedule.write(day5)
+    header6 = schedule.write("\n\nSaturday:\n")
+    task6 = schedule.write(day6)
+    header7 = schedule.write("\n\nSunday:\n")
+    task7 = schedule.write(day7)
+
