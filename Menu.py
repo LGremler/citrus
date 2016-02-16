f5ce9da71a409e11c6e532b10ca1b0e189c9d5ec
Logan Gremler <logangremler@gmail.com>
2016-02-15 23:35:21 -0600

    Add Menu
    This is the menu that will appear whenever the user runs the program.

---
+++ Menu.py
@@ -0,0 +1,11 @@
+# A snazzy looking menu for Citrus
+
+# Author: Logan Gremler 2/15/16
+
+def main_menu():
+    print "~~~~~~~~    Citrus    ~~~~~~~~"
+    print " A simple schedule generator"
+    print ""
+    print "1) Create a new schedule"
+    print "2) View an existing schedule"
+    print ""
